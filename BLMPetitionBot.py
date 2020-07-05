c420ce39ab4340fc4a1c69a5a8e7c431ee5cf7ce
obheda12 <35583758+obheda12@users.noreply.github.com>
2020-07-04 20:43:33 -0500

    Update BLMPetitionBot.py

--- BLMPetitionBot.py
+++ BLMPetitionBot.py
@@ -1,7 +1,51 @@
-#Drivers for Chrome Headless Browser
+print(""" 
+ ______   _____     ____    ____                                              
+|_   _ \ |_   _|   |_   \  /   _|                                             
+  | |_) |  | |       |   \/   |                                               
+  |  __'.  | |   _   | |\  /| |                                               
+ _| |__) |_| |__/ | _| |_\/_| |_                                              
+|_______/|________||_____||_____|___  _________  _____   ___   ____  _____    
+|_   __ \|_   __  ||  _   _  ||_   _||  _   _  ||_   _|.'   `.|_   \|_   _|   
+  | |__) | | |_ \_||_/ | | \_|  | |  |_/ | | \_|  | | /  .-.  \ |   \ | |     
+  |  ___/  |  _| _     | |      | |      | |      | | | |   | | | |\ \| |     
+ _| |_    _| |__/ |   _| |_    _| |_    _| |_    _| |_\  `-'  /_| |_\   |_    
+|_____|  |________| _|_____|_ |_____|  |_____|  |_____|`.___.'|_____|\____|   
+|_   _ \   .'   `. |  _   _  |                                                
+  | |_) | /  .-.  \|_/ | | \_|                                                
+  |  __'. | |   | |    | |                                                    
+ _| |__) |\  `-'  /   _| |_                                                   
+|_______/  `.___.'   |_____|                                                  
+                                              
+""")
+
+
+
+print("""
+
+Author: Omar Bheda
+Version: 1.1
+Last Update: 6/04/2020
+
+The BLM Petition Bot, scrapes all change.org petitions from https://blacklivesmatters.carrd.co/
+and automates the signing of petitions. This is accomplished using selenium and human input emulation tactics
+incorporated into the script to avoid captcha. Currently 100+ petitions are signed in a single run of the script.
+If captcha persists to be an issue for you please email omar.bheda@gmail.com with a screenshot of the captcha and
+details of the petition upon which the captcha executed. 
+
+Finally here are some resources to donate to after signing (NOTE: DO NOT DONATE TO CHANGE.ORG, DONATIONS DO NOT GO 
+ENTIRELY TO BLM CAUSES):
+
+http://www.blacklivesmatterdmv.org/fund-the-movement/
+https://blacklivesmatters.carrd.co/#victims
+https://blacklivesmatters.carrd.co/#bail
+https://blacklivesmatters.carrd.co/#business
+https://blacklivesmatters.carrd.co/#org
+
+""")
+
+#Imports
 import time
 import random
-import numpy
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,51 +54,29 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 
+
 def set_viewport_size(driver, width, height):
     window_size = driver.execute_script("""
         return [window.outerWidth - window.innerWidth + arguments[0],
           window.outerHeight - window.innerHeight + arguments[1]];
         """, width, height)
-    driver.set_window_size(*window_size) 
-
-# Randomization Related
-MIN_RAND = 0.64
-MAX_RAND = 1.27
-LONG_MIN_RAND = 4.78
-LONG_MAX_RAND = 11.1
-
-def random_wait (a, b):
-    rand = random.uniform(a, b)
-    time.sleep(rand)
+    driver.set_window_size(*window_size)
 
 #SETUP
 
-# Headless
+# Set Options / Uncomment "# opts.headless = True" in order to allow headless browsing
 opts = webdriver.FirefoxOptions()
-# # opts.headless = True
+# opts.headless = True
 
-#Firefox Profile
-driver = webdriver.FirefoxProfile()
-driver.set_preference("dom.webdriver.enabled", False)
-driver.set_preference('useAutomationExtension', False)
-driver.update_preferences()
-
-# Set Driver
-driver = webdriver.Chrome(executable_path="./chromedriver")
-#driver = webdriver.Firefox(executable_path="./geckodriver", options=opts)
+# Set Driver and Adjust Viewport to Avoid Captcha
+driver = webdriver.Firefox(executable_path="./geckodriver", options=opts)
 set_viewport_size(driver, 800, 600)
 driver.get('https://blacklivesmatters.carrd.co/#more')
-
 
 # Get Change.org Petitions
 search_text = "www.change.org"
 links = driver.find_elements_by_xpath('//a[contains(@href, "%s")]' % search_text)
 petition_list = []
-petition_list_no_captcha = []
-
-# Get No Captcha Petitions List from File
-with open('petitions_list.txt', 'r') as f:
-    petition_list_no_captcha = [line.strip() for line in f]
 
 # Set Count of Petitions
 count = 0
@@ -73,7 +95,7 @@ def SignPetitions():
     # Banner Displaying Petitions
     print("------- Below are the petitions that will be signed -------")
     print("")
-    for petition in petition_list_no_captcha:
+    for petition in petition_list:
         print(petition)
     print("")
 
@@ -87,7 +109,7 @@ def SignPetitions():
     total_count = 0
 
     # Iterate Through Petitions
-    for petition in petition_list_no_captcha:
+    for petition in petition_list:
 
         # Open Petition Page and Avoid Session Timeout
         driver.delete_all_cookies()
@@ -95,7 +117,7 @@ def SignPetitions():
         driver.refresh()
 
         # sleep avoid RC
-        random_wait(MIN_RAND,MAX_RAND)
+        time.sleep(5.0)
 
         # Username Input
         try:
@@ -105,7 +127,7 @@ def SignPetitions():
             pass
 
         # Sleep to prevent race conditions
-        random_wait(MIN_RAND,MAX_RAND)
+        time.sleep(.0)
         # Password Input
         try:
             Last_Name = driver.find_element_by_id('lastName')
@@ -114,7 +136,7 @@ def SignPetitions():
             pass
 
         # Sleep to prevent race conditions
-        random_wait(MIN_RAND,MAX_RAND)
+        time.sleep(1.0)
 
         # Email Input
         try:
@@ -124,14 +146,14 @@ def SignPetitions():
             pass
 
         # Sleep to prevent race conditions
-        random_wait(MIN_RAND,MAX_RAND)
+        time.sleep(2.0)
 
         # Submit Petition
         page = driver.find_element_by_xpath("//body")
         page.send_keys(Keys.RETURN)
 
         # Sleep to prevent race conditions
-        random_wait(LONG_MIN_RAND,LONG_MAX_RAND)
+        time.sleep(5.0)
 
         #Check to see if confirmation happened
 
@@ -146,7 +168,7 @@ def SignPetitions():
         driver.delete_all_cookies()
 
         # Sleep to prevent race conditions
-        random_wait(LONG_MIN_RAND,LONG_MAX_RAND)
+        time.sleep(5.0)
 
         # Sign_Button = driver.find_element_by_xpath('//*[@type="submit"]')
         # Sign_Button.click()
